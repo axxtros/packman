52b57c2650697c1ee508a3d031fb146470639340
ghofer <gabor.hofer@frontsoft.hu>
2017-05-04 15:02:55 +0200

    Ext.

--- pm/pm.cpp
+++ pm/pm.cpp
@@ -1,7 +1,11 @@
 // pm.cpp : Simple packman game console application.
+
+//sample:
 //https://www.youtube.com/watch?v=bfR-Rx4oEn8
 //unicode characters:
 //http://ascii-table.com/ascii-extended-pc-list.php
+//console colors:
+//http://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
 
 #include "stdafx.h"
 
